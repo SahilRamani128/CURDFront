8713f3ea63bf9aa7d648315033e50b10919ee69d
SahilRamani128 <sahilramani128@gmail.com>
2024-05-30 12:16:09 +0530

    Add files via upload

---
+++ src/component/TaskForm.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const TaskForm = ({ handleSubmit, handleOnChange, handleClose, rest, users }) => {
+  return (
+    <div className="addContainer">
+      <form onSubmit={handleSubmit}>
+        <div className="close-btn" onClick={handleClose}>×</div>
+        <label htmlFor="title">Title: </label>
+        <input type="text" id="title" name="title" onChange={handleOnChange} value={rest.title} />
+
+        <label htmlFor="description">Description: </label>
+        <textarea id="description" name="description" onChange={handleOnChange} value={rest.description} />
+
+        <label htmlFor="assignedUsers">Assign to Users: </label>
+        <select multiple={true} id="assignedUsers" name="assignedUsers" onChange={handleOnChange} value={rest.assignedUsers}>
+          {users.map(user => (
+            <option key={user._id} value={user._id}>{user.name} ({user.email})</option>
+          ))}
+        </select>
+
+        <button className="btn">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default TaskForm;
